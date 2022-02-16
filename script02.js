4f0633ee3801aad8a2b38a7c4ef7cfd0a92c0cf7
DevFrog92 <yongsu1481@gmail.com>
2022-02-16 12:18:00 +0900

    complete chapter02

--- script02.js
+++ script02.js
@@ -203,3 +203,221 @@ obs1$.pipe(
     err => console.error(err),
     _ => console.log('COMPLETE')
 )
+
+const { fromEvent } = rxjs
+const { ajax } = rxjs.ajax
+const { timeout, pluck } = rxjs.operators
+
+/* fromEvent(document, 'click').pipe(
+    // 주어진 시간안에 다음 값이 미발행 시 오류를 발생시킨다.
+    timeout(3000)
+).subscribe(
+    _ => console.log('OK'),
+    err => console.error(err)) */
+
+ajax('http://127.0.0.1:3000/people/name/random').pipe(
+    pluck('response'),
+    timeout(2000)
+).subscribe(console.log, console.error)
+
+const { fromEvent, interval, of } = rxjs
+const { ajax } = rxjs.ajax
+const { timeoutWith, pluck, scan } = rxjs.operators
+
+/* fromEvent(document, 'click').pipe(
+    timeoutWith(3000, interval(1000)),
+    // 위에서 나오는 값
+    scan((acc, x) => { return acc + 1 }, 0)
+).subscribe(console.log) */
+
+ajax('http://127.0.0.1:3000/people/name/random').pipe(
+    pluck('response'),
+    // 시간안에 응답이 오지 않으면 다른 값을 발행한다.
+    timeoutWith(2000, of({
+        id: 0,
+        first_name: 'Hong',
+        last_name: 'Gildong',
+        role: 'substitute'
+    }))
+).subscribe(console.log, console.error)
+
+// debounce
+
+const { fromEvent } = rxjs
+const { timeInterval, pluck, scan, tap } = rxjs.operators
+const { debounceTime } = rxjs.operators
+
+const clicks$ = fromEvent(document, 'click').pipe(
+    timeInterval(),
+    pluck('interval'),
+    scan((acc, i) => acc + i, 0),
+    tap(x => console.log('CLICKED: ' + x)),
+    debounceTime(1000)
+).subscribe(x => console.log('OUTPUT: -------- ' + x))
+
+
+// auditTime
+const { fromEvent } = rxjs
+const { timeInterval, pluck, scan, tap } = rxjs.operators
+const { debounceTime } = rxjs.operators
+
+const clicks$ = fromEvent(document, 'click').pipe(
+    timeInterval(),
+    pluck('interval'),
+    scan((acc, i) => acc + i, 0),
+    tap(x => console.log('CLICKED: ' + x)),
+)
+
+const { auditTime } = rxjs.operators
+
+clicks$.pipe(
+		// time duration 뒤에 마지막으로 입력된 값을 발행
+    auditTime(1000)
+).subscribe(x => console.log('OUTPUT: -------- ' + x))
+
+
+const { fromEvent } = rxjs
+const { timeInterval, pluck, scan, tap } = rxjs.operators
+const { debounceTime } = rxjs.operators
+
+const clicks$ = fromEvent(document, 'click').pipe(
+    timeInterval(),
+    pluck('interval'),
+    scan((acc, i) => acc + i, 0),
+    tap(x => console.log('CLICKED: ' + x)),
+)
+
+const { sampleTime } = rxjs.operators
+
+clicks$.pipe(
+		// 특정 시간 마다 값을 발행하고 싶을 떄 사용한다.
+    sampleTime(1000),
+    timeInterval()
+).subscribe(x => console.log('OUTPUT: -------- ' + x.value + ' :' + x.interval))
+
+
+const { fromEvent } = rxjs
+const { timeInterval, pluck, scan, tap } = rxjs.operators
+const { debounceTime } = rxjs.operators
+
+const clicks$ = fromEvent(document, 'click').pipe(
+    timeInterval(),
+    pluck('interval'),
+    scan((acc, i) => acc + i, 0),
+    tap(x => console.log('CLICKED: ' + x)),
+)
+
+const { throttleTime } = rxjs.operators
+
+clicks$.pipe(
+// leading: true의 경우 각 time duration의 첫 값을 뽑아 준다. 뒤에 나오는 값은 무시
+// trailing: true의 경우 반대
+// undefined -> schedular
+// audittime과 차이점은 마지막 complete에서 값을 반환하는 지, 하지 않는지에 대한 차이가 있다.
+    throttleTime(1000, undefined, {
+    	leading: true, trailing: true
+    })
+).subscribe(x => console.log('OUTPUT: -------- ' + x))
+
+
+const { of } = rxjs
+const { ajax } = rxjs.ajax
+const { concatMap, pluck } = rxjs.operators
+
+of(3, 15, 4, 9, 1, 7).pipe(
+// mergeMap과 concatMap의 차이는 '순서가 고려되어야 하는가?'이다.
+    concatMap(keyword => ajax(
+            `http://127.0.0.1:3000/people/${keyword}`
+        ).pipe(
+            pluck('response', 'first_name')
+        )
+    )
+).subscribe(console.log)
+
+const { range } = rxjs
+const { bufferCount } = rxjs.operators
+
+range(1, 100).pipe(
+// 몇개씩 자를 것인지, 몇개씩 건너뛸 건지
+  bufferCount(10, 10)
+).subscribe(console.log)
+
+
+const { of } = rxjs
+const { every } = rxjs.operators
+
+of(1, 3, 5, 7, 9, 11, 13, 15).pipe(
+// 모두 특정 기준에 부합하는지
+  every(x => x % 2 !== 0)
+).subscribe(console.log)
+
+const { fromEvent, timer } = rxjs
+const { defaultIfEmpty, pluck, takeUntil } = rxjs.operators
+
+fromEvent(document, 'click').pipe(
+  takeUntil(timer(5000)),
+  pluck('x'),
+  // 발행이 되지 않았을 떄, 기본 값을 발행
+  defaultIfEmpty('NO CLICK')
+).subscribe(console.log)
+
+const { iif, fromEvent, of } = rxjs
+const { pluck } = rxjs.operators
+
+fromEvent(document.querySelector('#check'), 'change').pipe(
+  pluck('target', 'checked')
+).subscribe(checked => {
+  iif(
+    _ => checked,
+    of('CHECKED'),
+    // false경우 지정해 주지 않으면 빈 스트림을 반환한다.
+  ).subscribe(
+    console.log,
+    err => console.log(err),
+    _ => console.log('COMPLETE')
+  )
+})
+
+const { empty } = rxjs
+
+// 스트림을 처리할 때, 값은 반환하지 않는 상황에서 사용한다.
+empty().subscribe(console.log, console.error, _ => console.log('COMPLETE'))
+
+const { interval } = rxjs
+const { take, tap, takeLast, share } = rxjs.operators
+
+const obs$ = interval(1000).pipe(
+  take(20),
+  tap(x => console.log(`side effect: ${x}`)),
+  // 중간에 조인한다.
+  share()
+)
+
+obs$.subscribe(x => console.log(`subscriber 1: ${x}`))
+
+setTimeout(_ => {
+  obs$.subscribe(x => console.log(`subscriber 2: ${x}`))
+}, 5000)
+setTimeout(_ => {
+  obs$.subscribe(x => console.log(`subscriber 3: ${x}`))
+}, 10000)
+
+
+const { interval } = rxjs
+const { take, tap, takeLast, share } = rxjs.operators
+
+const obs$ = interval(1000).pipe(
+  take(20),
+  tap(x => console.log(`side effect: ${x}`)),
+  // 중간에 조인한다. observable을 subject처럼 만들어 준다.
+  share()
+)
+
+obs$.subscribe(x => console.log(`subscriber 1: ${x}`))
+
+setTimeout(_ => {
+  obs$.subscribe(x => console.log(`subscriber 2: ${x}`))
+}, 5000)
+setTimeout(_ => {
+  obs$.subscribe(x => console.log(`subscriber 3: ${x}`))
+}, 10000)
